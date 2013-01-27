cf95632944b0ac4344a017af98552b55e8001c89
Chris Wilson <chris@cjetech.co.uk>
2013-01-27 10:14:57 +0100

    Removing sublime stiff

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,22 +2,12 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "init" --exclude "Sublime Text 2" -av . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "init" -av . ~
     if [ -f ~/.extra ]; then
         echo "These changes have been made in the .extra.dist file, please handle these manually"
         diff .extra.dist ~/.extra
     else
         mv ~/.extra.dist ~/.extra
-    fi
-    if [ -d ~/Library ]; then
-        if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages-old ]; then
-            mv ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages-old
-            mv ~/Library/Application\ Support/Sublime\ Text\ 2/Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Packages-old
-            mv ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages-old
-            ln -sf `pwd`/Sublime\ Text\ 2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages
-            ln -sf `pwd`/Sublime\ Text\ 2/Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-            ln -sf `pwd`/Sublime\ Text\ 2/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages
-        fi
     fi
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
